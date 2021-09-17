f794b5b36eabee32992c6b130c224922800e6f23
Hamza Kaced <hamzayoucefkaced@gmail.com>
2021-09-17 16:08:51 -0400

    initial commit

---
+++ src/main/java/com/hackthenorth/priceisthegoat/PriceisthegoatApplication.java
@@ -0,0 +1,13 @@
+package com.hackthenorth.priceisthegoat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceisthegoatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceisthegoatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hackthenorth/priceisthegoat/PriceisthegoatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackthenorth.priceisthegoat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceisthegoatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
