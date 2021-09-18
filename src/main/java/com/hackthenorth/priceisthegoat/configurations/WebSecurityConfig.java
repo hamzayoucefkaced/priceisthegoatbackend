5e2bde62924d269991cc473d94415d99f8da0aa2
josh <josh.fried13@gmail.com>
2021-09-18 16:05:00 -0400

    added local fe url

--- src/main/java/com/hackthenorth/priceisthegoat/configurations/WebSecurityConfig.java
+++ src/main/java/com/hackthenorth/priceisthegoat/configurations/WebSecurityConfig.java
@@ -18,15 +18,16 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
-    private static final String               FRONTEND_URL = "https://htn2021frontend.herokuapp.com/";
-    private final        HTNUserDetailService userDetailsService;
+    private static final String FRONTEND_URL       = "https://htn2021frontend.herokuapp.com/";
+    private static final String LOCAL_FRONTEND_URL = "http://localhost:3000";
+
+    private final HTNUserDetailService userDetailsService;
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
@@ -75,7 +76,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
     @Bean
     static CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList(FRONTEND_URL));
+        configuration.setAllowedOrigins(Arrays.asList(LOCAL_FRONTEND_URL, FRONTEND_URL));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
         configuration.setExposedHeaders(Arrays.asList("Authorization", "content-type"));
         configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
